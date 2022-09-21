8219850f9bc15d3404875a5d91fa9949c392611f
ihuy <10687596+ihuy@user.noreply.gitee.com>
2022-09-21 23:20:03 +0800

    Initial Commit

---
+++ oneSwim/oneSwimApp.swift
@@ -0,0 +1,17 @@
+//
+//  oneSwimApp.swift
+//  oneSwim
+//
+//  Created by jianye.liu on 2022/9/21.
+//
+
+import SwiftUI
+
+@main
+struct oneSwimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ oneSwimUITests/oneSwimUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  oneSwimUITestsLaunchTests.swift
+//  oneSwimUITests
+//
+//  Created by jianye.liu on 2022/9/21.
+//
+
+import XCTest
+
+class oneSwimUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
